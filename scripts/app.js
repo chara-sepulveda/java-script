1f44b514baedd549441fbf79b65181a76b618a10
Daniel Valencia <charaprogram19@gmail.com>
2022-07-18 09:26:32 -0500

    creamos una constante

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+let nombre=prompt("ingrese su nombre")
+let apellido=prompt("ingreseapellido")
+console.log(nombre+apellido)
+ 
+const email = "teykerychara16@gmail.com"
+console.log(`el correo es: ${email}`)
+
