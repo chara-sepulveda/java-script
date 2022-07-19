ad4fc9ad6853fc684a8a11cb141e5c7fbb561527
Daniel Valencia <charaprogram19@gmail.com>
2022-07-19 09:35:14 -0500

    este es para mirar el tipo de dato de la variable

--- clase2/scripts/index.js
+++ clase2/scripts/index.js
@@ -1,3 +1,8 @@
-console.log(age);
-console.log(nombre)
-let nombre="carlos"
+let age=5;
+let firstName="chara"
+let lastName="sepulveda"
+let isStudent=false
+let gender="male"
+let country=null
+
+console.log(typeof age)
