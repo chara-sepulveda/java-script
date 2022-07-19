facc2c0b1f09bc38cbb29d40fc1ff4fdf04b1fa4
Daniel Valencia <charaprogram19@gmail.com>
2022-07-19 06:58:40 -0500

    agregamos una carpeta para la creacion de  clase2

---
+++ clase1/scripts/app.js
@@ -0,0 +1,33 @@
+let nombre=prompt("ingrese su nombre")
+let apellido=prompt("ingreseapellido")
+console.log(nombre+apellido)
+ 
+const email = "teykerychara16@gmail.com"
+console.log(`el correo es: ${email}`)
+
+
+
+
+let gusto1=prompt("¿cual es el primer gusto?")
+let gusto2=prompt("¿cual es el segundo gusto?")
+let gusto3=prompt("¿cual es el tercer gusto?")
+alert(`Aquí tiene su helado de ${gusto1} ${gusto2} ${gusto3}`)
+
+
+let nombre=prompt("¿cual es tu nombre?")
+let apellido=prompt("¿cual es tu apellido?")
+let edad=prompt("¿cual es tu edad?")
+let nacionalidad=prompt("¿cual es tu nacionalidad?")
+let identificacion=prompt("¿cual es tu numero de indentifiacion personal?")
+alert(`el nuevo perfil ingresado al sistema: ${nombre} ${apellido} ${edad} ${nacionalidad} ${identificacion}`)
+
+let playlist=prompt("¿cual es el nombre de la playlist?")
+let cancion1=prompt("¿cual es la cancion?")
+let cancion2=promp("¿cual es la cancion?")
+let cancion3=promp("¿cual es la cancion?")
+alert(`Se ha creado la playlist ${playlist} con las canciones: ${cancion1}, ${cancion2} ${cancion3}`)
+
+
+
+
+
